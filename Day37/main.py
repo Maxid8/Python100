0e56a0934b634794edda43e79179329e516c57a1
Maxid8 <124875643+Maxid8@users.noreply.github.com>
2024-01-25 12:41:25 +0100

    before challenge

--- Day37/main.py
+++ Day37/main.py
@@ -1,7 +1,13 @@
 import requests
 
-USERNAME = "danika"
-TOKEN = "f1g5drt4jk1uzfhg8ikdr1g"
+# Create your own user and token. I outsourced this to a file called <Account.txt>. The first line of the file is the username and the second line is the token.
+file = open("Account.txt","r")
+content = file.readlines()
+
+USERNAME = content[0].strip("\n") 
+TOKEN = content[1]
+
+file.close()
 
 pixela_endpint = "https://pixe.la/v1/users"
 
@@ -12,21 +18,22 @@ user_params = {
     "notMinor": "yes"
 }
 
-response = requests.post(url=pixela_endpint, json=user_params)
+# response = requests.post(url=pixela_endpint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Page Read",
+    "unit": "page",
+    "type": "int",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
-
-# graph_endpoint = f"{pixela_endpint}/{USERNAME}/graphs"
-
-# graph_config = {
-#     "id": "graph1",
-#     "name": "Page Read",
-#     "unit": "page",
-#     "type": "int",
-#     "color": "sora"
-# }
-
-# headers = {
-#     "X-"
-# }
-
-# requests.post()
