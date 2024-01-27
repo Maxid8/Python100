22cafc9a525a1e8103f2ed428b2331a32cc893b9
Maxid8 <124875643+Maxid8@users.noreply.github.com>
2024-01-27 16:08:06 +0100

    before final challenge

--- Day37/main.py
+++ Day37/main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 # Create your own user and token. I outsourced this to a file called <Account.txt>. The first line of the file is the username and the second line is the token.
 file = open("Account.txt","r")
@@ -17,11 +18,13 @@ user_params = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
 
 # response = requests.post(url=pixela_endpint, json=user_params)
 # print(response.text)
 
-graph_endpoint = f"{pixela_endpint}/{USERNAME}/graphs"
 
 graph_config = {
     "id": "graph1",
@@ -31,9 +34,20 @@ graph_config = {
     "color": "sora"
 }
 
-headers = {
-    "X-USER-TOKEN": TOKEN
+# graph_endpoint = f"{pixela_endpint}/{USERNAME}/graphs"
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+time_tuple = time.localtime() # get struct_time
+time_string = time.strftime("%Y%m%d", time_tuple)
+
+graph_endpoint = f"{pixela_endpint}/{USERNAME}/graphs/{graph_config['id']}"
+
+graph_update = {
+    "date": time_string,
+    "quantity": "10"
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+response = requests.post(url=graph_endpoint, json=graph_update, headers=headers)
 print(response.text)
